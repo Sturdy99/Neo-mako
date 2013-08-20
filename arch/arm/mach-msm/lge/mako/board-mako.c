27eca1216d41b181c8fc2b8b6908e96a17171c58
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-08-20 05:24:28 +0100

    Revert SPM changes
    Revert "mako: Add power collapse debug counters support."
    
    This reverts commit 134dabf16ce710f3d45628b6ead9860568502116.
    
    Revert "mako: Fix SPM sequences"
    
    This reverts commit 47c24d5551c025ea3f0ea6faead5605f4417508b.
    
    Revert "mako: Initialize SPM driver earlier than thermal"
    
    This reverts commit 4f3615d7315bb51bf9a8952b78a5ce99f2a0993b.
    
    Revert "msm: board-8064: Initialize SPM driver earlier than thermal"
    
    This reverts commit e612a31d05ea4bf8924f5af0a9f9e42aec74bf8f.
    
    Revert "mako: Update spm retention sequence to support per core retention"
    
    This reverts commit 9422feb5d54d7aefc4c151ca6c93cfd06fd58ed1.
    
    Revert "msm: pm: Update spm retention sequence to support per core retention"
    
    This reverts commit 590fce53503e46d3d9ba002a71cb259640b1c99d.
    
    Revert "mako: Enable Krait Retention."
    
    This reverts commit bc9742a2ad2857cd827427f412bf94c0bb2cf45d.
    
    Revert "mako: Add support to query cpu status"
    
    This reverts commit f8494838bf99f63e7b8548af2b59fe3d8c067e92.
    
    Revert "msm: pm: Add support to query cpu status"
    
    This reverts commit 1f9269180b8d2f2b8a4eb7527b2fc2b27e4ebf9a.
    
    Revert "msm: pm-data: Enable powercollapse/suspend_enabled for non-boot cpus"
    
    This reverts commit 533b3ffddb7bfb143c22512fd891c1213413cfa5.
    
    Revert "msm: pm: Add support for retention on each core"
    
    This reverts commit faa5907835b303330d8b1fe33775f4e0ca99a61e.
    
    Revert "msm: hotplug: wait for hotplug completion event from secondary core"
    
    This reverts commit 03488d46c6bf03302283faf38941240197522ec8.
    
    msm: spm: set vdd directly for current cpu
    
    In cases where a cpu is setting vdd for itself we need not
    call msm_spm_smp_set_vdd through smp_call_function_single.
    
    Without this change when CPU calls for itself when interrupts
    are disabled we get following warning
    
    [ 38.407752] ------------[ cut here ]------------
    [ 38.407752] WARNING: at kernel/kernel/smp.c:320
    smp_call_function_single+0xa8/0x1c8()
    [ 38.407752] Modules linked in: adsprpc
    [ 38.407752] [<c00149b8>] (unwind_backtrace+0x0/0x11c) from [<c0082afc>]
    (warn_slowpath_common+0x4c/0x64)
    [ 38.407752] [<c0082afc>] (warn_slowpath_common+0x4c/0x64) from
    [<c0082b2c>] (warn_slowpath_null+0x18/0x1c)
    [ 38.407752] [<c0082b2c>] (warn_slowpath_null+0x18/0x1c) from
    [<c00c510c>] (smp_call_function_single+0xa8/0x1c8)
    [ 38.407752] [<c00c510c>] (smp_call_function_single+0xa8/0x1c8) from
    [<c0058f6c>] (msm_spm_set_vdd+0x48/0x70)
    [ 38.407752] [<c0058f6c>] (msm_spm_set_vdd+0x48/0x70) from [<c0056a98>]
    (msm_pm_power_collapse+0x13c/0x334)
    [ 38.407752] [<c0056a98>] (msm_pm_power_collapse+0x13c/0x334) from
    [<c0057120>] (msm_pm_enter+0x170/0x2dc)
    [ 38.407752] [<c0057120>] (msm_pm_enter+0x170/0x2dc) from [<c00b7580>]
    (suspend_devices_and_enter+0x1d0/0x334)
    [ 38.407752] [<c00b7580>] (suspend_devices_and_enter+0x1d0/0x334) from
    [<c00b7800>] (pm_suspend+0x11c/0x1f8)
    [ 38.407752] [<c00b7800>] (pm_suspend+0x11c/0x1f8) from [<c00b87a4>]
    (suspend+0x68/0x180)
    [ 38.407752] [<c00b87a4>] (suspend+0x68/0x180) from [<c009b050>]
    (process_one_work+0x280/0x488)
    [ 38.407752] [<c009b050>] (process_one_work+0x280/0x488) from
    [<c009b46c>] (worker_thread+0x214/0x3b4)
    [ 38.407752] [<c009b46c>] (worker_thread+0x214/0x3b4) from [<c009f364>]
    (kthread+0x84/0x90)
    [ 38.407752] [<c009f364>] (kthread+0x84/0x90) from [<c000f108>]
    (kernel_thread_exit+0x0/0x8)
    [ 38.407752] ---[ end trace da227214a82491ba ]---
    
    Change-Id: I1aa121f57d74860b6bc8620b40699567e55a9d02
    CRs-Fixed: 480408
    Signed-off-by: Venkat Devarasetty <vdevaras@codeaurora.org>

--- arch/arm/mach-msm/lge/mako/board-mako.c
+++ arch/arm/mach-msm/lge/mako/board-mako.c
@@ -1390,13 +1390,6 @@ static struct msm_rpmrs_level msm_rpmrs_levels[] = {
 	},
 
 	{
-		MSM_PM_SLEEP_MODE_RETENTION,
-		MSM_RPMRS_LIMITS(ON, ACTIVE, MAX, ACTIVE),
-		true,
-		415, 715, 340827, 475,
-	},
-
-	{
 		MSM_PM_SLEEP_MODE_POWER_COLLAPSE_STANDALONE,
 		MSM_RPMRS_LIMITS(ON, ACTIVE, MAX, ACTIVE),
 		true,
@@ -1496,18 +1489,6 @@ static uint8_t spm_power_collapse_without_rpm[] __initdata = {
 	0x24, 0x30, 0x0f,
 };
 
-static uint8_t spm_retention_cmd_sequence[] __initdata = {
-	0x00, 0x05, 0x03, 0x0D,
-	0x0B, 0x00, 0x0f,
-};
-
-static uint8_t spm_retention_with_krait_v3_cmd_sequence[] __initdata = {
-	0x42, 0x1B, 0x00,
-	0x05, 0x03, 0x0D, 0x0B,
-	0x00, 0x42, 0x1B,
-	0x0f,
-};
-
 static uint8_t spm_power_collapse_with_rpm[] __initdata = {
 	0x00, 0x24, 0x54, 0x10,
 	0x09, 0x07, 0x01, 0x0B,
@@ -1515,45 +1496,18 @@ static uint8_t spm_power_collapse_with_rpm[] __initdata = {
 	0x24, 0x30, 0x0f,
 };
 
-static struct msm_spm_seq_entry msm_spm_boot_cpu_seq_list[] __initdata = {
+static struct msm_spm_seq_entry msm_spm_seq_list[] __initdata = {
 	[0] = {
 		.mode = MSM_SPM_MODE_CLOCK_GATING,
 		.notify_rpm = false,
 		.cmd = spm_wfi_cmd_sequence,
 	},
 	[1] = {
-		.mode = MSM_SPM_MODE_POWER_RETENTION,
-		.notify_rpm = false,
-		.cmd = spm_retention_cmd_sequence,
-	},
-	[2] = {
 		.mode = MSM_SPM_MODE_POWER_COLLAPSE,
 		.notify_rpm = false,
 		.cmd = spm_power_collapse_without_rpm,
 	},
-	[3] = {
-		.mode = MSM_SPM_MODE_POWER_COLLAPSE,
-		.notify_rpm = true,
-		.cmd = spm_power_collapse_with_rpm,
-	},
-};
-static struct msm_spm_seq_entry msm_spm_nonboot_cpu_seq_list[] __initdata = {
-	[0] = {
-		.mode = MSM_SPM_MODE_CLOCK_GATING,
-		.notify_rpm = false,
-		.cmd = spm_wfi_cmd_sequence,
-	},
-	[1] = {
-		.mode = MSM_SPM_MODE_POWER_RETENTION,
-		.notify_rpm = false,
-		.cmd = spm_retention_cmd_sequence,
-	},
 	[2] = {
-		.mode = MSM_SPM_MODE_POWER_COLLAPSE,
-		.notify_rpm = false,
-		.cmd = spm_power_collapse_without_rpm,
-	},
-	[3] = {
 		.mode = MSM_SPM_MODE_POWER_COLLAPSE,
 		.notify_rpm = true,
 		.cmd = spm_power_collapse_with_rpm,
@@ -1618,12 +1572,12 @@ static struct msm_spm_platform_data msm_spm_data[] __initdata = {
 		.reg_init_values[MSM_SPM_REG_SAW2_AVS_HYSTERESIS] = 0x00,
 #endif
 		.reg_init_values[MSM_SPM_REG_SAW2_SPM_CTL] = 0x01,
-		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DLY] = 0x03020004,
-		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DATA_0] = 0x0084009C,
-		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DATA_1] = 0x00A4001C,
+		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DLY] = 0x02020204,
+		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DATA_0] = 0x0060009C,
+		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DATA_1] = 0x0000001C,
 		.vctl_timeout_us = 50,
-		.num_modes = ARRAY_SIZE(msm_spm_boot_cpu_seq_list),
-		.modes = msm_spm_boot_cpu_seq_list,
+		.num_modes = ARRAY_SIZE(msm_spm_seq_list),
+		.modes = msm_spm_seq_list,
 	},
 	[1] = {
 		.reg_base_addr = MSM_SAW1_BASE,
@@ -1633,12 +1587,12 @@ static struct msm_spm_platform_data msm_spm_data[] __initdata = {
 		.reg_init_values[MSM_SPM_REG_SAW2_AVS_HYSTERESIS] = 0x00,
 #endif
 		.reg_init_values[MSM_SPM_REG_SAW2_SPM_CTL] = 0x01,
-		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DLY] = 0x03020004,
-		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DATA_0] = 0x0084009C,
-		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DATA_1] = 0x00A4001C,
+		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DLY] = 0x02020204,
+		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DATA_0] = 0x0060009C,
+		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DATA_1] = 0x0000001C,
 		.vctl_timeout_us = 50,
-		.num_modes = ARRAY_SIZE(msm_spm_nonboot_cpu_seq_list),
-		.modes = msm_spm_nonboot_cpu_seq_list,
+		.num_modes = ARRAY_SIZE(msm_spm_seq_list),
+		.modes = msm_spm_seq_list,
 	},
 	[2] = {
 		.reg_base_addr = MSM_SAW2_BASE,
@@ -1648,12 +1602,12 @@ static struct msm_spm_platform_data msm_spm_data[] __initdata = {
 		.reg_init_values[MSM_SPM_REG_SAW2_AVS_HYSTERESIS] = 0x00,
 #endif
 		.reg_init_values[MSM_SPM_REG_SAW2_SPM_CTL] = 0x01,
-		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DLY] = 0x03020004,
-		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DATA_0] = 0x0084009C,
-		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DATA_1] = 0x00A4001C,
+		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DLY] = 0x02020204,
+		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DATA_0] = 0x0060009C,
+		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DATA_1] = 0x0000001C,
 		.vctl_timeout_us = 50,
-		.num_modes = ARRAY_SIZE(msm_spm_nonboot_cpu_seq_list),
-		.modes = msm_spm_nonboot_cpu_seq_list,
+		.num_modes = ARRAY_SIZE(msm_spm_seq_list),
+		.modes = msm_spm_seq_list,
 	},
 	[3] = {
 		.reg_base_addr = MSM_SAW3_BASE,
@@ -1663,12 +1617,12 @@ static struct msm_spm_platform_data msm_spm_data[] __initdata = {
 		.reg_init_values[MSM_SPM_REG_SAW2_AVS_HYSTERESIS] = 0x00,
 #endif
 		.reg_init_values[MSM_SPM_REG_SAW2_SPM_CTL] = 0x01,
-		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DLY] = 0x03020004,
-		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DATA_0] = 0x0084009C,
-		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DATA_1] = 0x00A4001C,
+		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DLY] = 0x02020204,
+		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DATA_0] = 0x0060009C,
+		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DATA_1] = 0x0000001C,
 		.vctl_timeout_us = 50,
-		.num_modes = ARRAY_SIZE(msm_spm_nonboot_cpu_seq_list),
-		.modes = msm_spm_nonboot_cpu_seq_list,
+		.num_modes = ARRAY_SIZE(msm_spm_seq_list),
+		.modes = msm_spm_seq_list,
 	},
 };
 
@@ -1857,10 +1811,8 @@ static struct platform_device *common_devices[] __initdata = {
 static struct platform_device *cdp_devices[] __initdata = {
 	&msm_device_sps_apq8064,
 #ifdef CONFIG_MSM_ROTATOR
-        &msm_rotator_device,
+       &msm_rotator_device,
 #endif
-        &msm8064_pc_cntr,
-	&msm8064_cpu_slp_status,
 };
 
 #ifdef CONFIG_USER_RC_INPUT
@@ -1976,36 +1928,11 @@ static void __init register_i2c_devices(void)
 #endif
 }
 
-static void __init apq8064ab_update_retention_spm(void)
-{
-	int i;
-
-	/* Update the SPM sequences for krait retention on all cores */
-	for (i = 0; i < ARRAY_SIZE(msm_spm_data); i++) {
-		int j;
-		struct msm_spm_platform_data *pdata = &msm_spm_data[i];
-		for (j = 0; j < pdata->num_modes; j++) {
-			if (pdata->modes[j].cmd ==
-					spm_retention_cmd_sequence)
-				pdata->modes[j].cmd =
-					spm_retention_with_krait_v3_cmd_sequence;
-		}
-	}
-}
-
 static void __init apq8064_common_init(void)
 {
 	struct msm_rpmrs_level rpmrs_level;
 
 	platform_device_register(&msm_gpio_device);
-	if (cpu_is_krait_v3()) {
-		msm_pm_set_tz_retention_flag(0);
-		apq8064ab_update_retention_spm();
-	} else {
-		msm_pm_set_tz_retention_flag(1);
-	}
-	msm_spm_init(msm_spm_data, ARRAY_SIZE(msm_spm_data));
-	msm_spm_l2_init(msm_spm_l2_data);
 	msm_tsens_early_init(&apq_tsens_pdata);
 	msm_thermal_init(&msm_thermal_pdata);
 	if (socinfo_init() < 0)
@@ -2057,6 +1984,8 @@ static void __init apq8064_common_init(void)
 	slim_register_board_info(apq8064_slim_devices,
 		ARRAY_SIZE(apq8064_slim_devices));
 	apq8064_init_dsps();
+	msm_spm_init(msm_spm_data, ARRAY_SIZE(msm_spm_data));
+	msm_spm_l2_init(msm_spm_l2_data);
 	BUG_ON(msm_pm_boot_init(&msm_pm_boot_pdata));
 }
 
